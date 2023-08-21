718b82d5b4ff596a930a87c3fe3e8607ab2b9804
Arvind Varma <93394422+Arvind78@users.noreply.github.com>
2023-08-21 03:51:00 -0700

    Update Signup.jsx

--- src/components/signup/Signup.jsx
+++ src/components/signup/Signup.jsx
@@ -107,7 +107,7 @@ const Signup = () => {
       return false;
     }
     setLoading(true)
-    axios.post("https://chatgpt-4fg0.onrender.com/api/signup", signupDeteils)
+    axios.post("https://chatgptserver-tibl.onrender.com/api/signup", signupDeteils)
 
       .then((res) => {
         setLoading(false)
