9b6da8a598941fd0f9688977c2bfa4080f6bd878
Arvind <av894883@gmail.com>
2023-10-27 07:22:36 -0700

    something

--- src/redux/userSlice.js
+++ src/redux/userSlice.js
@@ -1,34 +1,34 @@
 import { createSlice } from '@reduxjs/toolkit'
 
 const initialState = {
-     user:null,
-     sucess:false,
-  }
+   user: null,
+   sucess: false,
+}
 
-  const userSlice = createSlice({
-    name:"user",
-    initialState,
-    reducers:{
-       signupSccuess :(state,action)=>{
-        state.user = action.payload
-        state.sucess=true
-       },
-       loginSccuess :(state,action)=>{
-        state.user = action.payload
-        state.sucess=true
-       },
-       logout :(state)=>{
-        state.user = null
-        state.sucess=false
-       },
-       updateSccuess :(state,action)=>{
-        state.user = action.payload
-        state.sucess=true
-       }
+const userSlice = createSlice({
+   name: "user",
+   initialState,
+   reducers: {
+      signupSccuess: (state, action) => {
+         state.user = action.payload
+         state.sucess = true
+      },
+      loginSccuess: (state, action) => {
+         state.user = action.payload
+         state.sucess = true
+      },
+      logout: (state) => {
+         state.user = null
+         state.sucess = false
+      },
+      updateSccuess: (state, action) => {
+         state.user = action.payload
+         state.sucess = true
+      }
 
-    }
-  })
+   }
+})
 
-  export const {signupSccuess,loginSccuess,updateSccuess,logout} =userSlice.actions;
+export const { signupSccuess, loginSccuess, updateSccuess, logout } = userSlice.actions;
 
-  export default userSlice.reducer;
+export default userSlice.reducer;
