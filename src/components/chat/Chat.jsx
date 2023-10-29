0772dcbd1e8c3985d16790c1ad9f44dd60add063
Arvind <av894883@gmail.com>
2023-10-29 01:34:30 -0700

    somethigChange

--- src/components/chat/Chat.jsx
+++ src/components/chat/Chat.jsx
@@ -26,9 +26,10 @@ const Chat =()=>{
 const chatgtpHandler =()=>{
   setLoading(true)
    axios.post("https://chatgptserver-tibl.onrender.com/api/chat",{prompt}).then((res)=>{
+    const originalString =  res.data.result
     
-     setResult(res.data.result)
-     setLoading(false)
+      setResult(originalString)
+      setLoading(false)
 
    }).catch((err)=>{
   setLoading(false)
@@ -100,12 +101,13 @@ const logoutHandler =()=>{
                         < Avatar src="https://img.icons8.com/color/48/circled-user-male-skin-type-3--v1.png" size={45} />
                     <div  >
                         
-                          <pre style={{textAlign:"justify"}}>
+                       
                           <p style={{textAlign:"justify"}}>
+                            <pre>
                           {result}
+                          </pre>
                           </p>
 
-                          </pre>
                     </div>
                   
                 </div>
@@ -120,7 +122,7 @@ const logoutHandler =()=>{
                 < Avatar src="https://img.icons8.com/color/48/circled-user-male-skin-type-3--v1.png" size={45} />
                     <input type="text" onChange={(e)=>setPrompt(e.target.value)} value={prompt} placeholder="write your queries..." />
                   {(!loading)?
-                  <AiOutlineSend size={25} className={chat.sendIcon} onClick={chatgtpHandler}/>:
+                  <AiOutlineSend size={30} className={chat.sendIcon} onClick={chatgtpHandler}/>:
 
                    <ReactLoading type={"spin"} color={"green"} height={'20px'} width={'20px'} />
                   }
@@ -202,11 +204,11 @@ const chatgtpHandler =()=>{
                     <div className={chat.chatResultInfoM}>
                             < Avatar src="https://img.icons8.com/color/48/circled-user-male-skin-type-3--v1.png" size={45} />
                         <div  >
-                         <p>
+                         <pre>
                         
                           {result}
                     
-                          </p>
+                          </pre>
                         </div>
                      
                     </div>
